385f254f1eaacf176672a867c9a54f5cdc4fb4af
Max <maxorozco213@gmail.com>
2019-11-06 20:34:49 -0800

    Finalizing random number generator functions; Moving on to integrating the die roller

--- MultiApp/app/src/main/java/com/example/multiapp/RandomNumberFragment.kt
+++ MultiApp/app/src/main/java/com/example/multiapp/RandomNumberFragment.kt
@@ -1,9 +1,12 @@
 package com.example.multiapp
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.EditText
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -17,25 +20,28 @@ class RandomNumberFragment: Fragment() {
 
         val binding = DataBindingUtil.inflate<FragmentRandnumBinding>(
             inflater, R.layout.fragment_randnum, container, false)
-
         binding.randNumGen = this
+
+        val lowerBound: EditText = binding.lowerBoundEntry
+        val upperBound: EditText = binding.upperBoundEntry
+        val randNumView = binding.generatedNumber
+        val usedNumbers: ArrayList<Int> = arrayListOf()
 
         binding.getNumberButton.setOnClickListener @Suppress("UNUSED_ANONYMOUS_PARAMETER")
         { view: View ->
+            Log.i("CLICKED", "On Click is working")
             try {
-                val lowerInt: Int = binding.upperBoundEntry.text.toString().toInt()
+                val lowerInt: Int = binding.lowerBoundEntry.text.toString().toInt()
                 val upperInt: Int = binding.upperBoundEntry.text.toString().toInt()
                 val valueRange: IntRange = (lowerInt..upperInt)
-                val usedNumbers: ArrayList<Int> = arrayListOf()
-                val checked: Boolean = binding.withReplaceCheckBox.isChecked
-                val randNumView = binding.generatedNumber
+                val checked: CheckBox = binding.withReplaceCheckBox
                 var randomInt = valueRange.random()
                 var isUnique = true
                 print(usedNumbers)
 
                 when (checked) {
                     checked -> {
-                        if (checked) {
+                        if (checked.isChecked) {
                             while (usedNumbers.contains(randomInt)) {
                                 isUnique = true
                                 randomInt = valueRange.random()
@@ -61,6 +67,13 @@ class RandomNumberFragment: Fragment() {
                 print("")
             }
 
+            binding.withReplaceCheckBox.setOnClickListener @Suppress("UNUSED_ANONYMOUS_PARAMETER")
+            { view1: View ->
+                lowerBound.text.clear()
+                upperBound.text.clear()
+                randNumView.text = ""
+                Log.i("ISCHECKED VAL", binding.withReplaceCheckBox.isChecked.toString())
+            }
         }
         return binding.root
     }
