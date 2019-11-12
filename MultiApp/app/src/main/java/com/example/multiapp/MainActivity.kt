430ac71ccc5bffd657ed1ed610369b10a559d71a
Maximilian Orozco <maxorozco213@gmail.com>
2019-11-11 21:07:33 -0800

    Created empty navigation drawer

--- MultiApp/app/src/main/java/com/example/multiapp/MainActivity.kt
+++ MultiApp/app/src/main/java/com/example/multiapp/MainActivity.kt
@@ -2,22 +2,35 @@ package com.example.multiapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.example.multiapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         val navController = this.findNavController(R.id.myNavHostFragment)
+        binding.activityMain = this
+        drawerLayout = binding.drawerLayout
+
         NavigationUI.setupActionBarWithNavController(this, navController)
-            .setupWithNavController(navController)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+//        navController.setNavigationItemSelectedListener { menuItem ->
+//            when ( menuItem.itemId) -> {
+//
+//            }
+//        }
     }
+
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
